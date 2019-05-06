aff3bc3f78c85fd6570018ceaa1370e7370771d3
robin2810 <robinbehrendt@web.de>
2019-05-06 14:52:26 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,4 @@
 <?php
-if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-  $uri = 'https://';
-} else {
-  $uri = 'http://';
-}
-$uri .= $_SERVER['HTTP_HOST'];
-header('Location: '.$uri.'/php-2019/project-barney');
+header('Location: /php-2019/project-barney');
 exit;
 ?>
